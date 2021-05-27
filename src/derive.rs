5fe656b9509e0f899b23fc799aa04500c6475038
hoping <baihaoping@gmail.com>
2021-05-27 19:47:05 +0800

    modify derive sample code

--- src/derive.rs
+++ src/derive.rs
@@ -1,24 +1,22 @@
-use crate::{UserInfo, BagInfo};
-use specs::{World, DispatcherBuilder, WorldExt, LazyUpdate};
+use crate::{BagInfo, DynamicManager, UserInfo};
 use codegen::system;
+use specs::{DispatcherBuilder, LazyUpdate, World, WorldExt};
 
 #[system]
 #[dynamic(user)]
-fn user_derive(_user:&UserInfo, _bag:&BagInfo, #[state] _other:&usize) {
-
-}
+fn user_derive(_user: &UserInfo, _bag: &BagInfo, #[state] _other: &usize) {}
 
 #[system]
 #[dynamic(lib = "guild", fn = "test")]
-fn guild_derive(_user:&UserInfo, _bag:&BagInfo, #[resource] _lazy_update:&LazyUpdate) {
-
-}
+fn guild_derive(_user: &UserInfo, _bag: &BagInfo, #[resource] _lazy_update: &LazyUpdate) {}
 
 pub fn run() {
     let mut world = World::new();
-    let builder = DispatcherBuilder::new();
-    //user_derive_setup(&mut world, &mut builder);
-    //guild_derive_setup(&mut world, &mut builder);
+    let mut builder = DispatcherBuilder::new();
+    let dm = DynamicManager::default();
+    //UserDeriveSystem::default().setup(&mut world, &mut builder, &dm);
+    //GuildDeriveSystem::default().setup(&mut world, &mut builder, &dm);
+    world.insert(dm);
     let mut dispatcher = builder.build();
     dispatcher.setup(&mut world);
     dispatcher.dispatch(&world);
