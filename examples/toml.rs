468511e6d3a9e4acde974a17eb0c2fc19910b2a0
hoping <baihaoping@gmail.com>
2021-06-15 11:14:21 +0800

    config file support multiple messages

--- examples/toml.rs
+++ examples/toml.rs
@@ -1,4 +1,4 @@
-use ecs_engine::config::Config;
+use ecs_engine::config::{Config, ConfigFile};
 use serde_derive::{Deserialize, Serialize};
 use std::{fs::File, io::Read};
 
@@ -8,6 +8,6 @@ fn main() {
         .unwrap()
         .read_to_string(&mut data)
         .unwrap();
-    let data: Config = toml::from_str(data.as_str()).unwrap();
+    let data: ConfigFile = toml::from_str(data.as_str()).unwrap();
     println!("{:?}", data);
 }
